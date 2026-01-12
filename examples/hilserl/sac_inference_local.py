4d5a8ffd818110ed12ee99137cc71b091fc12861
mxy <mengxyokok@gmail.com>
2026-01-12 10:47:08 +0800

    perfect: rl local infer

--- examples/hilserl/sac_inference_local.py
+++ examples/hilserl/sac_inference_local.py
@@ -4,11 +4,12 @@
 import torch
 from lerobot.configs.train import TrainRLServerPipelineConfig
 from lerobot.policies.factory import make_policy, make_pre_post_processors
-from lerobot.envs.utils import preprocess_observation
 import gymnasium as gym
 import gym_hil
 
 from lerobot.rl.gym_manipulator import make_processors, make_robot_env
+from lerobot.processor.converters import create_transition
+from lerobot.processor.core import TransitionKey
 
 # 配置checkpoint路径
 checkpoint_path = "outputs/train/2026-01-05/16-33-40_franka_pick_cube_sim_sac_baseline/checkpoints/010000/pretrained_model"
@@ -25,28 +26,34 @@ policy = make_policy(cfg=cfg.policy, env_cfg=cfg.env)
 policy.eval()
 policy.to(device)
 
-
 # 创建环境、环境处理器和动作处理器
 env, teleop_device = make_robot_env(cfg.env)
 env_processor, action_processor = make_processors(env, teleop_device, cfg.env, device)
 env_processor.reset()
 action_processor.reset()
 
-obs, _ = env.reset()
+obs, info = env.reset()
 
 while True:
-    # 将环境观察转换为LeRobot格式
-    # 环境返回: {"pixels": {"front": ..., "wrist": ...}, "agent_pos": ...}
-    # 需要转换为: {"observation.images.front": ..., "observation.images.wrist": ..., "observation.state": ...}
-
-    processed_obs=env_processor(obs)
+     # 处理观察
+    transition = create_transition(observation=obs, info=info)
+    transition = env_processor(transition)
+    processed_obs = transition[TransitionKey.OBSERVATION]
+    
+    # 推理
     with torch.no_grad():
-        # 推理
         action = policy.select_action(processed_obs)
 
-    processed_action=action_processor(action)
-    
+    # 创建动作transition并处理
+    action_transition = create_transition(action=action)
+    processed_action_transition = action_processor(action_transition)
+    processed_action = processed_action_transition[TransitionKey.ACTION]
+
+    # env step
     obs, reward, terminated, truncated, info = env.step(processed_action)
+    
     if terminated or truncated:
-        obs, _ = env.reset()
+        obs, info = env.reset()
+    
+
 
